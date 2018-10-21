f5a95dcf1cb954c1db311b608a6cf31bf86a0300
Andrei Borissenko <AndreiBoris@users.noreply.github.com>
2018-10-21 13:42:20 -0400

    Update json-feed.js

--- feed/json-feed.js
+++ feed/json-feed.js
@@ -1,7 +1,7 @@
 {
   "uid": "urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b",
-  "updateDate": "2016-05-23T22:34:51.0Z",
-  "titleText": "Amazon Developer Blog, week in review May 23rd",
+  "updateDate": "2018-10-20T22:34:51.0Z",
+  "titleText": "Testing tested bits",
   "mainText": "",
   "streamUrl": "https://developer.amazon.com/public/community/blog/myaudiofile.mp3",
   "redirectionUrl": "https://developer.amazon.com/public/community/blog"
